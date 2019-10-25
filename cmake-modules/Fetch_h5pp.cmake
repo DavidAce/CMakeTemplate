0905a67b474eab4f1fe720a96f7477e93e2175c6
DavidAce <aceituno@kth.se>
2019-10-25 11:46:31 +0200

    Moving to configure-time build of dependencies

--- cmake-modules/Fetch_h5pp.cmake
+++ cmake-modules/Fetch_h5pp.cmake
@@ -1,11 +1,11 @@
 
-find_package(h5pp PATHS ${INSTALL_DIRECTORY}/h5pp $ENV{H5PP_DIR})
+find_package(h5pp PATHS ${INSTALL_DIR_THIRD_PARTY}/h5pp $ENV{H5PP_DIR})
 
 if(h5pp_FOUND AND TARGET h5pp::h5pp)
     message(STATUS "h5pp FOUND IN SYSTEM: ${H5PP_ROOT}")
 #    add_library(h5pp ALIAS h5pp::h5pp)
 else()
-    message(STATUS "h5pp will be installed into ${INSTALL_DIRECTORY}/h5pp")
+    message(STATUS "h5pp will be installed into ${INSTALL_DIR_THIRD_PARTY}/h5pp")
     include(cmake-modules/BuildThirdParty.cmake)
     build_third_party(
             "h5pp"
@@ -14,7 +14,7 @@ else()
             "${INSTALL_DIR_THIRD_PARTY}"
             "-DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}"
     )
-    find_package(h5pp PATHS ${INSTALL_DIRECTORY}/h5pp $ENV{H5PP_DIR})
+    find_package(h5pp PATHS ${INSTALL_DIR_THIRD_PARTY}/h5pp $ENV{H5PP_DIR})
     if(h5pp_FOUND AND TARGET h5pp::h5pp)
         message(STATUS "h5pp installed successfully: ${H5PP_ROOT}")
         else()
