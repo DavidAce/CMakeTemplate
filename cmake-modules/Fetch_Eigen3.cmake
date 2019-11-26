94af63837a2bfc4e7138f1a037faca4b8eb297c5
DavidAce <aceituno@kth.se>
2019-11-27 00:25:32 +0100

    Adding Conan package manager support

--- cmake-modules/Fetch_Eigen3.cmake
+++ cmake-modules/Fetch_Eigen3.cmake
@@ -11,26 +11,45 @@ find_package(Eigen3 3.3.7
 if(TARGET Eigen3::Eigen)
     message(STATUS "Eigen3 found")
 elseif (DOWNLOAD_MISSING)
-    message(STATUS "Eigen3 will be installed into ${EXTERNAL_INSTALL_DIR}/Eigen3 on first build.")
-    include(cmake-modules/BuildExternalLibs.cmake)
-    build_external_libs(
-            "Eigen3"
-            "${EXTERNAL_CONFIG_DIR}"
-            "${EXTERNAL_BUILD_DIR}"
-            "${EXTERNAL_INSTALL_DIR}"
-            ""
-    )
-    message("Looking in: ${EXTERNAL_INSTALL_DIR}/Eigen3/share/eigen3/cmake")
-    find_package(Eigen3 3.3.7
-            NO_DEFAULT_PATH NO_CMAKE_PACKAGE_REGISTRY
-            PATHS ${EXTERNAL_INSTALL_DIR}/Eigen3/share/eigen3/cmake)
-    if(Eigen3_FOUND AND TARGET Eigen3::Eigen)
+    # We get the package for the user
+    if(DOWNLOAD_METHOD_CMAKE)
+        # We do it good ol' way with externalprojectadd
+        message(STATUS "Eigen3 will be installed into ${EXTERNAL_INSTALL_DIR}/Eigen3 on first build.")
+        include(cmake-modules/BuildExternalLibs.cmake)
+        build_external_libs(
+                "Eigen3"
+                "${EXTERNAL_CONFIG_DIR}"
+                "${EXTERNAL_BUILD_DIR}"
+                "${EXTERNAL_INSTALL_DIR}"
+                ""
+        )
+        message("Looking in: ${EXTERNAL_INSTALL_DIR}/Eigen3/share/eigen3/cmake")
+        find_package(Eigen3 3.3.7
+                NO_DEFAULT_PATH NO_CMAKE_PACKAGE_REGISTRY
+                PATHS ${EXTERNAL_INSTALL_DIR}/Eigen3/share/eigen3/cmake)
+    elseif(DOWNLOAD_METHOD_CONAN)
+        # New fancy way with conan. Requires conan, obviously
+        conan_add_remote(NAME conan-dmrg INDEX 1
+                URL http://thinkstation.duckdns.org:8081/artifactory/api/conan/conan-dmrg)
+        conan_cmake_run(REQUIRES eigen/3.3.7@conan-dmrg/patched
+                BASIC_SETUP CMAKE_TARGETS
+                BUILD missing)
+        add_library (Eigen3::Eigen INTERFACE IMPORTED)
+        target_link_libraries(Eigen3::Eigen INTERFACE CONAN_PKG::Eigen3)
+    else()
+        message(FATAL_ERROR "Requested download of missing library [Eigen3] but no download method selected")
+    endif()
+
+    # Report if package found or not
+    if(TARGET Eigen3::Eigen)
         message(STATUS "Eigen3 installed successfully")
     else()
         message(STATUS "config_result: ${config_result}")
         message(STATUS "build_result: ${build_result}")
         message(FATAL_ERROR "Eigen3 could not be downloaded.")
     endif()
+
+
 
 else()
     message(FATAL_ERROR "Dependency Eigen3 not found and DOWNLOAD_MISSING is OFF")
--- cmake-modules/Fetch_spdlog.cmake
+++ cmake-modules/Fetch_spdlog.cmake
@@ -9,25 +9,41 @@ if(spdlog_FOUND AND TARGET spdlog::spdlog)
     message(STATUS "spdlog found")
 
 elseif (DOWNLOAD_MISSING)
-    message(STATUS "Spdlog will be installed into ${EXTERNAL_INSTALL_DIR}/spdlog on first build.")
-    include(cmake-modules/BuildExternalLibs.cmake)
-    build_external_libs(
-            "spdlog"
-            "${EXTERNAL_CONFIG_DIR}"
-            "${EXTERNAL_BUILD_DIR}"
-            "${EXTERNAL_INSTALL_DIR}"
-            ""
-    )
-    message("Looking in: ${EXTERNAL_INSTALL_DIR}/spdlog/${CMAKE_INSTALL_LIBDIR}/cmake/spdlog")
-    find_package(spdlog 1.3 NO_DEFAULT_PATH PATHS ${EXTERNAL_INSTALL_DIR}/spdlog/${CMAKE_INSTALL_LIBDIR}/cmake/spdlog)
-    if(spdlog_FOUND AND TARGET spdlog::spdlog)
+    # We do it good ol' way with externalprojectadd
+    if(DOWNLOAD_METHOD_CMAKE)
+
+        message(STATUS "Spdlog will be installed into ${EXTERNAL_INSTALL_DIR}/spdlog on first build.")
+        include(cmake-modules/BuildExternalLibs.cmake)
+        build_external_libs(
+                "spdlog"
+                "${EXTERNAL_CONFIG_DIR}"
+                "${EXTERNAL_BUILD_DIR}"
+                "${EXTERNAL_INSTALL_DIR}"
+                ""
+        )
+        message("Looking in: ${EXTERNAL_INSTALL_DIR}/spdlog/${CMAKE_INSTALL_LIBDIR}/cmake/spdlog")
+        find_package(spdlog 1.3 NO_DEFAULT_PATH PATHS ${EXTERNAL_INSTALL_DIR}/spdlog/${CMAKE_INSTALL_LIBDIR}/cmake/spdlog)
+
+    elseif(DOWNLOAD_METHOD_CONAN)
+        # New fancy way with conan. Requires conan, obviously
+        include(conan-modules/conan.cmake)
+        conan_cmake_run(REQUIRES spdlog/1.4.2@bincrafters/stable
+                BASIC_SETUP CMAKE_TARGETS
+                BUILD missing)
+        add_library (spdlog::spdlog INTERFACE IMPORTED)
+        target_link_libraries(spdlog::spdlog INTERFACE CONAN_PKG::spdlog)
+    else()
+        message(FATAL_ERROR "Requested download of missing library [spdlog] but no download method selected")
+    endif()
+
+    # Report if package found or not
+    if(TARGET spdlog::spdlog)
         message(STATUS "spdlog installed successfully")
     else()
         message(STATUS "config_result: ${config_result}")
         message(STATUS "build_result: ${build_result}")
         message(FATAL_ERROR "Spdlog could not be downloaded.")
     endif()
-
 else()
     message(FATAL_ERROR "Dependency spdlog not found and DOWNLOAD_MISSING is OFF")
 
