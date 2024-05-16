189f5d3ffe2f443c3cffa32a57d778bf39752421
DavidAce <aceituno@kth.se>
2024-05-16 08:25:07 +0200

    Updated mirror action

--- tests/test-h5pp.cpp
+++ tests/test-h5pp.cpp
@@ -1,16 +1,26 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 
-#include <h5pp/h5pp.h>
 #include <doctest/doctest.h>
+#include <h5pp/h5pp.h>
 
-
-std::vector<int> write_and_read(const std::vector<int> &vec) {
+template<typename T>
+T write_then_read(const T &item) {
     auto f = h5pp::File("test-h5pp.h5", h5pp::FileAccess::REPLACE);
-    f.writeDataset(vec, "vec");
-    return f.readDataset<std::vector<int>>("vec");
+    f.writeDataset(item, h5pp::type::sfinae::type_name<T>());
+    return f.readDataset<T>(h5pp::type::sfinae::type_name<T>());
 }
 
-TEST_CASE("std::vector<int>") {
-    auto vec = std::vector<int>{1, 2, 3};
-    CHECK(write_and_read(vec) == vec);
+TEST_CASE("write/read std::vector<int>") {
+        auto vec = std::vector{1, 2, 3};
+        CHECK(vec == write_then_read(vec));
 }
+TEST_CASE("write/read std::vector<double>") {
+    auto vec = std::vector{1.0, 2.0, 3.0};
+    CHECK(write_then_read(vec) == vec);
+}
+TEST_CASE("write/read std::vector<complex>") {
+    using namespace std::complex_literals;
+    auto vec = std::vector{1.0 + 1.0i, 2.0 + 2.0i, 2.0 + 2.0i};
+    CHECK(write_then_read(vec) == vec);
+}
+
